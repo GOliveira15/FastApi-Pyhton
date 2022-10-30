c065f2918031b6ca864ac67d8d93fab3eea21a71
GOliveira15 <gabrielosilva15@gmail.com>
2022-10-30 17:45:39 -0300

    FastAPI CRUD: CORS error fixed

--- Products/main.py
+++ Products/main.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
+    "https://web-productsapp.azurewebsites.net",
 ]
 
 app.add_middleware(
